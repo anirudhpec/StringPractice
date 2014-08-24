d802f6858576968925984b01b088d72c3ab1bfa8
Anirudh <anirudhsethi@Anirudhs-MacBook-Pro.local>
2014-08-24 15:48:05 -0500

    :NONE:NONE - Initial Commit.

--- StringPractice/ASViewController.h
+++ StringPractice/ASViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ASViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *myButton;
 
 @end
--- StringPractice/ASViewController.m
+++ StringPractice/ASViewController.m
@@ -18,6 +18,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *myString = @"This is just a practice and time pass with Objective C and hope it is fruitful";
+    NSArray *wordsInSentence  = [myString componentsSeparatedByString:@" "];
+    NSMutableArray *capitalizedWords  = [[NSMutableArray alloc] init];
+    for (int word = 0; word< [wordsInSentence count]; word ++) {
+        NSString *unCapitalizedWord = [wordsInSentence objectAtIndex:word];
+        NSString *capitalizedWord = [unCapitalizedWord capitalizedString];
+        [capitalizedWords addObject:capitalizedWord];
+    }
+    NSLog(@"%@", capitalizedWords);
+    
+    [self.myButton setTitle:@"Wow, somehow" forState:UIControlStateNormal];
+    [self.myButton setHidden:YES];
+    UIAlertView *alerView =[[UIAlertView alloc] initWithTitle:@"Alert!" message:@"This is an error message" delegate:Nil cancelButtonTitle:@"Cancel" otherButtonTitles : Nil];
+    [alerView show];
+
 }
 
 - (void)didReceiveMemoryWarning
